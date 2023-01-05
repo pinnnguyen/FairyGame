bcd1d0c733f4ca4d6828c71ff6604efcd9141235
pinnnguyen <pinnguyen0597@gmail.com>
2023-01-05 09:06:05 +0700

    Update role.ts

--- middleware/role.ts
+++ middleware/role.ts
@@ -1,12 +1,17 @@
 export default defineNuxtRouteMiddleware(async () => {
   const { loadPlayer } = usePlayerStore()
 
-  const role = await $fetch('/api/player', {
-    headers: (useRequestHeaders(['cookie']) as any),
-  })
+  try {
+    const role = await $fetch('/api/player', {
+      headers: (useRequestHeaders(['cookie']) as any),
+    })
 
-  if (role?.player?.sid) {
-    loadPlayer(role)
-    return navigateTo('/')
+    if (role?.player?.sid) {
+      loadPlayer(role)
+      return navigateTo('/')
+    }
+  }
+  catch (error) {
+    console.error(error)
   }
 })
